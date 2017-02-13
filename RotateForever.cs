4bf0a16db12fe0fb4a4f84594165f8ad4c58f843
Evil <codeconcubine@126.com>
2017-02-13 15:59:57 +0800

    some scripts

---
+++ RotateForever.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateForever : MonoBehaviour {
+
+	public enum Space
+	{
+		Vector_up,
+		transform_up,
+		Custom_Point,
+		Custom_Axis
+	}
+
+	public bool isRotate = true;
+	// 周期
+	[Range(0.1f,10)]
+	public float timeOfRotateOnePeriod = 1;
+	// 使用本地坐标还是世界坐标
+	public Space space = Space.Custom_Axis;
+	[SerializeField]
+	private Transform down;
+	[SerializeField]
+	private Transform up;
+	[SerializeField]
+	private Vector3 axis;
+
+	// 每次转动的角度
+	private float anglePer;
+	// 转动的总的角度
+	private float allAngle = 360;
+	// 每帧的时间
+	private float timePer = 0.016f;
+	// 初始旋转角度
+	private Quaternion initialRotation;
+
+
+	// Use this for initialization
+	void Start () {
+		anglePer = allAngle * timePer / timeOfRotateOnePeriod;
+		initialRotation = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isRotate) {
+			Vector3 axis;
+			switch (space) {
+			case Space.Vector_up:
+				axis = Vector3.up;
+				break;
+			case Space.transform_up:
+				axis = transform.up;
+				break;
+			case Space.Custom_Point:
+				axis = up.position - down.position;
+				break;
+			case Space.Custom_Axis:
+				axis = this.axis;
+				break;
+			default:
+				axis = Vector3.up;
+				break;
+			}
+			transform.RotateAround (transform.position, axis, anglePer);
+		}
+	}
+}
---
+++ RotateForeverEditor.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(RotateForever))]
+public class RotateForeverEditor : Editor {
+	private SerializedObject obj;
+
+	private SerializedProperty isRotate;
+	private SerializedProperty timeOfRotateOnePeriod;
+	private SerializedProperty space;
+	private SerializedProperty up;
+	private SerializedProperty down;
+	private SerializedProperty axis;
+
+	void OnEnable()
+	{
+		obj = new SerializedObject (target);
+		isRotate = obj.FindProperty ("isRotate");
+		timeOfRotateOnePeriod = obj.FindProperty ("timeOfRotateOnePeriod");
+		space = obj.FindProperty ("space");
+		up = obj.FindProperty ("up");
+		down = obj.FindProperty ("down");
+		axis = obj.FindProperty ("axis");
+	}
+
+	public override void OnInspectorGUI()
+	{
+		obj.Update ();
+
+		EditorGUILayout.PropertyField (isRotate, new GUIContent ("是否旋转"));
+		EditorGUILayout.PropertyField (timeOfRotateOnePeriod, new GUIContent ("旋转一圈需要的时间"));
+		EditorGUILayout.PropertyField (space, new GUIContent ("围绕的旋转轴"));
+		EditorGUILayout.LabelField ("当前围绕轴为：");
+		if (space.enumValueIndex == 0) {
+			EditorGUILayout.LabelField ("      世界坐标系的上");
+		}
+		else if (space.enumValueIndex == 1) {
+			EditorGUILayout.LabelField ("       它自身的上");
+		}
+		else if (space.enumValueIndex == 2) {
+			EditorGUILayout.PropertyField (up, new GUIContent("上顶点"));
+			EditorGUILayout.PropertyField (down, new GUIContent("下顶点"));
+		} else if(space.enumValueIndex == 3){
+			EditorGUILayout.PropertyField (axis);
+		}
+
+		obj.ApplyModifiedProperties();
+	}
+
+}
